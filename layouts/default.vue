2a754ace9165fc5d6e40fbd7534ccee4a4134cb1
Sander Moolin <sandermoolin@gmail.com>
2019-05-17 10:38:04 -0400

    Initial

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="fragment-shader">
+        <h2>export bug demo</h2>
+        <canvas class="canvas" ref="canvas" />
+    </div>
+</template>
+
+<script>
+// import Phenomenon from 'phenomenon-px'
+
+export default {
+    mounted() {
+        let fragment = ''
+
+        const uniforms = {}
+
+        const render = () => {}
+
+        // kick loop
+        // Phenomenon(fragment, uniforms, render, this.$refs.canvas)
+    }
+}
+</script>
